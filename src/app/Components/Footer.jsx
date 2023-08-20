afd30950dccdb480d603dc33d89698530f990825
danilo2504 <danilobautista2004@gmail.com>
2023-08-20 15:11:22 +0200

    CU-861n7p1q3 - crear side bars

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { socialLinks } from "../Data/socialLinks";
 import { optionTexts } from "../Data/optionsTexts";
-import { socialLinks } from "../Data/SocialLinks";
 import ListOption from "./Common/ListOption";
+import SocialItem from "./Common/SocialItem";
 
 function Footer() {
-  const SocialItem = ({ path, svg: SvgIcon }) => {
-    return (
-      <li>
-        <Link rel="noopener noreferrer" href={path}>
-          <SvgIcon />
-        </Link>
-      </li>
-    );
-  };
-
   return (
-    <div className="flex justify-center">
+    <div id="footer" className="flex justify-center z-40">
       <div className="bg-secondary h-[250px] w-full mx-auto px-32 py-12 max-w-[1512px] flex flex-col justify-between">
         <div className="flex justify-between items-start">
           <Link href="#">
@@ -28,7 +19,7 @@ function Footer() {
               alt="Logo de portafolio"
             />
           </Link>
-          <ul className="max-w-[540px] w-full flex justify-between items-center">
+          <ul className="max-w-[460px] w-full flex justify-between items-center">
             {socialLinks.map(({ id, path, svg }) => {
               return <SocialItem key={id} path={path} svg={svg} />;
             })}
